03edeeae1291fc87c0226fcde79306b189356e00
ricofa <ricof.afandi@gmail.com>
2021-03-08 16:34:24 +0700

    chg: signup

---
+++ lib/services/auth_services.dart
@@ -0,0 +1,18 @@
+part of 'services.dart';
+
+class AuthServices {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
+  static Future signUp(String email, String password, String name,
+      List<String> selectedGenres, String selectedLanguage) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+      User user = result.user.convertToUser(
+          name: name,
+          selectedLanguage: selectedLanguage,
+          selectedGenres: selectedGenres);
+    } catch (e) {}
+  }
+}
---
+++ lib/services/services.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mov_tix/models/models.dart';
+import 'package:mov_tix/extensions/extensions.dart';
+
+part 'auth_services.dart';
